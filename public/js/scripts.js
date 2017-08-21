61c5e8ac0f01c3186906927c70a1d6933789c209
stefan <stefan.emailacct@gmail.com>
2017-08-21 21:14:01 +0100

    added table x-close event

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -60,8 +60,19 @@ function openPopup(asof,rate,value,purchasevalue,pvday,pchange){
       }
       //¡» inserting popup
       var handle = document.querySelector('.sidever');
-      popup.innerHTML = ' <table id="popwrapper' + count + '"' + ' class="main" align="center" id="main" width="300" height="200" align="center" border="0" cellpadding="0" cellspacing="0"> <tr> <td valign="top" class="header">Currency: ' + rate +  '<div id="isX">x</div></td></tr><tr> <td valign="top" class="subheading">As of ' + asof  + '</td></tr><tr> <td valign="top" id="innerContainer"> <table cellpadding="0" cellspacing="0" border="1" width="100% bg="#f3f2f2"> <tr> <td valign="top" class="cktd"><label for="xchange">Exchange rate:</label><input name="xchange" type="text" value="'+ value +'" class="txbox"/></td><td valign="top" class="cktd"><label for="percent">$1USD:</label><input name="percentage" type="text" value="'+ purchasevalue +'"' + ' class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Previous day:</label><input name="pday" value="'+  pvday  + '" type="text" class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Day change:</label><input name="pday" value="'+  dchange  + '" type="text" class="txbox"/></td></tr></table> </td></tr></table> ';
+      popup.innerHTML = ' <table id="popwrapper' + count + '"' + ' class="main" align="center" id="main" width="300" height="200" align="center" border="0" cellpadding="0" cellspacing="0"> <tr> <td valign="top" class="header">Currency: ' + rate +  '<div class="isX">x</div></td></tr><tr> <td valign="top" class="subheading">As of ' + asof  + '</td></tr><tr> <td valign="top" id="innerContainer"> <table cellpadding="0" cellspacing="0" border="1" width="100% bg="#f3f2f2"> <tr> <td valign="top" class="cktd"><label for="xchange">Exchange rate:</label><input name="xchange" type="text" value="'+ value +'" class="txbox"/></td><td valign="top" class="cktd"><label for="percent">$1USD:</label><input name="percentage" type="text" value="'+ purchasevalue +'"' + ' class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Previous day:</label><input name="pday" value="'+  pvday  + '" type="text" class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Day change:</label><input name="pday" value="'+  dchange  + '" type="text" class="txbox"/></td></tr></table> </td></tr></table> ';
       handle.parentElement.appendChild(popup);
+      var victim;
+      document.getElementById('sidebar').
+        addEventListener('click',function(e) {
+            console.log(e);
+            debugger;
+            if(e.target.className == "isX") {
+              victim = e.target.parentElement.parentElement.parentElement.parentElement;
+              victim.parentElement.removeChild(victim);
+            }
+
+        },false);
         
 }//¡» openPopup fn
 
