0b036d413ad9cff43d4b4f527f1d30fda0617b60
stefan <stefan.emailacct@gmail.com>
2017-08-26 00:58:23 +0100

    fixed table removal bug

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -65,19 +65,33 @@ function openPopup(asof,rate,value,purchasevalue,pvday,pchange){
       popup.innerHTML = ' <table id="popwrapper' + count + '"' + ' class="main" align="center" id="main" width="300" height="200" align="center" border="0" cellpadding="0" cellspacing="0"> <tr> <td valign="top" class="header">Currency: ' + rate +  '<div class="isX">x</div></td></tr><tr> <td valign="top" class="subheading">As of ' + asof  + '</td></tr><tr> <td valign="top" id="innerContainer"> <table cellpadding="0" cellspacing="0" border="1" width="100% bg="#f3f2f2"> <tr> <td valign="top" class="cktd"><label for="xchange">Exchange rate:</label><input name="xchange" type="text" value="'+ value +'" class="txbox"/></td><td valign="top" class="cktd"><label for="percent">$1USD:</label><input name="percentage" type="text" value="'+ purchasevalue +'"' + ' class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Previous day:</label><input name="pday" value="'+  pvday  + '" type="text" class="txbox"/></td><td valign="top" class="cktd"><label for="pday">Day change:</label><input name="pday" value="'+  dchange  + '" type="text" class="txbox"/></td></tr></table> </td></tr></table> ';
       handle.parentElement.appendChild(popup);
       var victim;
+      function tableDisplay() {
+          //tables asof
+          var aos = document.querySelector('.subheading').textContent;
+          aos = aos.replace(/\D/g,'');
+          //current table
+          aos = aos.substr(0,2);
+          aos = Number(aos) -1;
+          //remove style
+          var remsty = document.querySelectorAll('.display img')[aos];
+          console.log(remsty);
+          remsty.style.background = "none";
+      }//tableDisplay
+
       document.getElementById('sidebar').
         addEventListener('click',function(e) { 
-            if(e.target.classList.length == 1) {
+            if(e.target.classList.length == 1 && e.target.className == "isX") {
+              console.log(e);
               victim = e.target.parentElement.parentElement.parentElement.parentElement;
+              tableDisplay(victim);
               console.log(victim);
               e.target.classList.remove('isX');
               victim.parentElement.removeChild(victim);
               count -= 1;
-              console.log(count);         
-            }
+              console.log(count);  
+                     
+            }//if
          
-         
-
         },false);
         
 }//¡» openPopup fn
@@ -105,12 +119,13 @@ document.getElementById('sidebar').addEventListener('click',function(e) {
 /*
 ¡ calls openPopup fn
 ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞*/
-debugger;
 ras.addEventListener('click',function(e) {
   console.log(e);
   if(e.target.className == "rates") {
       var pre = e.target.parentElement.parentElement.parentElement.parentElement || null;
-      //whatever-it-takes-approach
+      var cal = pre.parentElement.previousElementSibling.firstElementChild;
+      cal.style.background = 'rgba(255, 189, 90, 0.75)';
+      //first-data
       if(pre.previousElementSibling != null) {
         console.log(e);
         var abrate =  pre.previousElementSibling.innerText;
@@ -125,6 +140,7 @@ ras.addEventListener('click',function(e) {
         //remove leading zeros 1,2,3
         abrate = abrate.replace(/^0+/, '');
         abrate = Number(abrate);
+        console.log(abrate);
         
         //rates
         key = arr[abrate - 1];
